b7569b8460fe4535c8250623ab0684ad51dea906
Aluwani24 <160849668+Aluwani24@users.noreply.github.com>
2025-07-28 17:10:21 +0200

    added mobile resposive to the components

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Navbar.module.css'
-import { FaSearch } from 'react-icons/fa'
+import { FaSearch, FaBars, FaTimes } from 'react-icons/fa'
 import { ContentContainer } from '../ContentContainer'
 
 export const Navbar: React.FC = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
     return (
         <nav className={styles.nav}>
             <ContentContainer className={styles.content}>
                 <div className={styles.logo}>HYKROX</div>
-                <div className={styles.links}>
+                <div className={styles.hamburger} onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
+                    {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+                </div>
+                <div className={`${styles.links} ${isMobileMenuOpen ? styles.show : ''}`}>
                     <a href="#" className={styles.link}>HOME</a>
                     <a href="#designs" className={styles.link}>DESIGNS</a>
                     <a href="#services" className={styles.link}>SERVICES</a>
